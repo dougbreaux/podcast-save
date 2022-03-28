0bc690366655ffc16dde5cdd860bc9e5db259f7a
Doug Breaux <25640850+dougbreaux@users.noreply.github.com>
2022-03-27 20:20:45 -0500

    command-line arg for feed URL, no error on CTRL-C

--- get-episode-files.py
+++ get-episode-files.py
@@ -1,3 +1,4 @@
+import sys
 import feedparser
 import urllib.request
 import re
@@ -5,27 +6,35 @@ import datetime
 import time
 import os
 
-feed = feedparser.parse('https://heritagepark.org/podcast/1e3ea995-6640-464a-96d8-7e70eecdb18a.xml')
+# try/catch CTRL-C https://stackoverflow.com/questions/7073268/remove-traceback-in-python-on-ctrl-c
+try:
 
-for entry in feed.entries:
+    # command-line argument for feed URL
+    feed = feedparser.parse(sys.argv[1])
 
-    title = entry.title
-    fileUrl = entry.enclosures[0].href
-    
-    published = entry.published_parsed
-    year = published.tm_year
-    month = published.tm_mon
-    day = published.tm_mday
+    for entry in feed.entries:
 
-    # zero-pad single-digit month/day
-    filename = f'{year}-{month:02d}-{day:02d}-{title}.mp3'
+        title = entry.title
+        fileUrl = entry.enclosures[0].href
+        
+        published = entry.published_parsed
+        year = published.tm_year
+        month = published.tm_mon
+        day = published.tm_mday
 
-    # remove/replace illegal filename chars, like '?'
-    valid_filename = re.sub('[^\w_.)( -]', '', filename)
-    
-    print("Retrieving " + valid_filename)
-    urllib.request.urlretrieve(fileUrl, valid_filename)
-    
-    date=datetime.datetime(year=year, month=month, day=day)
-    modTime=time.mktime(date.timetuple())
-    os.utime(valid_filename, (modTime,modTime))
+        # zero-pad single-digit month/day
+        filename = f'{year}-{month:02d}-{day:02d}-{title}.mp3'
+
+        # remove/replace illegal filename chars, like '?'
+        valid_filename = re.sub('[^\w_.)( -]', '', filename)
+        
+        print("Retrieving " + valid_filename)
+        urllib.request.urlretrieve(fileUrl, valid_filename)
+        
+        date=datetime.datetime(year=year, month=month, day=day)
+        modTime=time.mktime(date.timetuple())
+        os.utime(valid_filename, (modTime,modTime))
+
+except KeyboardInterrupt:
+    print("Feed download interrupted")
+    sys.exit(0)
